bcce79893f4ef00765a88a0939e43d9fa5df2fb0
Salvador M <chava_r17@hotmail.com>
2024-10-03 20:28:52 -0600

    Hidden options of unused catalogs

--- Proyecto_TiendaSoft_WPF/TiendaSoft_WPF/Views/Page_Catalagos.xaml.vb
+++ Proyecto_TiendaSoft_WPF/TiendaSoft_WPF/Views/Page_Catalagos.xaml.vb
@@ -3,38 +3,35 @@
     Private _idPieza As String = ""
 
     Sub rootlayout_onLoaded() Handles rootLayout.Loaded
-        btn_Aseguradoras.IsEnabled = False
         If (_idPieza.Length > 0) Then
             'Product_Frame.Navigate(New Page_product_catalagoProducto(_idPieza))
         Else
-            Product_Frame.Navigate(New Catalagos_Modificar("ASEGURADORA"))
+            Product_Frame.Navigate(New Catalagos_Modificar("FAMILIAS"))
+            btn_grupos.IsEnabled = False
         End If
 
     End Sub
 
-    Sub buttonsClick(sender As Object, e As RoutedEventArgs) Handles btn_Aseguradoras.Click, btn_grupos.Click, btn_Marcas.Click, btn_tipos.Click, btn_Ubicaciones.Click
+    Sub buttonsClick(sender As Object, e As RoutedEventArgs) Handles btn_grupos.Click, btn_Ubicaciones.Click
         restaurarBotones()
         sender.IsEnabled = False
 
         Select Case sender.name
-            Case "btn_Aseguradoras"
-                Product_Frame.Navigate(New Catalagos_Modificar("ASEGURADORA"))
+            'Case "btn_Aseguradoras"
+            '    Product_Frame.Navigate(New Catalagos_Modificar("ASEGURADORA"))
             Case "btn_grupos"
                 Product_Frame.Navigate(New Catalagos_Modificar("FAMILIAS"))
-            Case "btn_Marcas"
-                Product_Frame.Navigate(New Catalagos_Modificar("MARCA"))
-            Case "btn_tipos"
-                Product_Frame.Navigate(New Catalagos_Modificar("TIPO"))
+            'Case "btn_Marcas"
+            '    Product_Frame.Navigate(New Catalagos_Modificar("MARCA"))
+            'Case "btn_tipos"
+            '    Product_Frame.Navigate(New Catalagos_Modificar("TIPO"))
             Case "btn_Ubicaciones"
                 Product_Frame.Navigate(New Catalagos_Modificar("UBICACIONES"))
         End Select
     End Sub
 
     Private Sub restaurarBotones()
-        btn_Aseguradoras.IsEnabled = True
         btn_grupos.IsEnabled = True
-        btn_Marcas.IsEnabled = True
-        btn_tipos.IsEnabled = True
         btn_Ubicaciones.IsEnabled = True
     End Sub
 
